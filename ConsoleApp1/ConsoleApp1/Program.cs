e8ca58bf574a26288d1f347a06b2a6e74ba0b069
s26697 <s26697@pjwstk.edu.pl>
2024-03-11 18:11:12 +0100

    Modyfikacja 1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,5 +4,17 @@
     {
         Console.WriteLine("Hello, World!");
     }
+
+    public static void AvgCalc(int[] tab)
+    {
+        int flex = 0;
+        foreach (int e in tab)
+        {
+            flex += e;
+        }
+        Console.WriteLine("Średnią w tablicy jest liczba: " + flex/tab.Length);
+        
+    }
+    
 }
 
