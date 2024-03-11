3093298bf5942711806f7692d63aed0afb862af8
s26697 <s26697@pjwstk.edu.pl>
2024-03-11 18:05:08 +0100

    initial project

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program()
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
 
-Console.WriteLine("Hello, World!");
