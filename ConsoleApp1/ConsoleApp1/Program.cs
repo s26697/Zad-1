d1cd59c145da85565bd836d27522db5b30e23c9d
s26697 <s26697@pjwstk.edu.pl>
2024-03-11 18:34:10 +0100

    Wykonalem feature

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,8 @@
     {
         int[] tab = { 1, 2, 3, 4, 5 };
        AvgCalc(tab); 
-
+        
+       MaxCalc(tab);
 
 
     }
@@ -19,6 +20,21 @@
         Console.WriteLine("Średnią w tablicy jest liczba: " + flex/tab.Length);
         
     }
-    
+   
+    public static void MaxCalc(int[] tab)
+    {
+        int flex = 0;
+        
+        foreach (int e in tab)
+        {
+            if (e > flex)
+            {
+                flex = e;
+            }
+            
+        }
+        
+        Console.WriteLine("Największa liczbą w tablicy jest: " + flex);
+    }
 }
 
